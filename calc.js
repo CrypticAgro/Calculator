d2757f95ba6da0ce13070226cba81778aa54e213
CrypticAgro <jtech3029@gmail.com>
2022-09-10 13:46:11 -0600

    created Calculator and parts of basic functionality

--- calc.js
+++ calc.js
@@ -14,3 +14,59 @@ function divide(numOne, numTwo){
     return numOne/numTwo;
 }
 
+var numObject = {numOne: [], numTwo: [],  numOperator: []};
+var finalNumObject = {numOne: [], numTwo: [],  numOperator: []};
+
+//hover effect
+
+const numContainers = document.getElementsByClassName("num")
+for (var i = 0 ; i < numContainers.length; i++) {
+    numContainers[i].addEventListener('mouseenter' , (e) => { 
+        e.target.style = ' background-color: gray;';
+        e.stopPropagation();
+    });
+    numContainers[i].addEventListener('mouseleave' , (e) => {
+        e.target.style = ' background-color: white;';
+        e.stopPropagation();
+    }); 
+// calculation getter
+    numContainers[i].addEventListener('click', (e) => {
+    let mommy = e.target.getAttribute("data-number");
+    numObject.numOne.push(mommy);
+
+    if(!mommy){
+        numObject.numOne.pop();
+        mommy = e.target.getAttribute("data-operator");
+        numObject.numOperator.push(mommy);
+    }
+    showNumOne(e);
+    })
+ }
+ 
+
+const cdContainers = document.getElementsByClassName("cd")
+for (var i = 0 ; i < cdContainers.length; i++) {
+    cdContainers[i].addEventListener('mouseenter' , (e) => { 
+        e.target.style = ' background-color: gray;';
+        e.stopPropagation();
+    });
+    cdContainers[i].addEventListener('mouseleave' , (e) => {
+        e.target.style = ' background-color: white;';
+        e.stopPropagation();
+    }); 
+    cdContainers[i].addEventListener('click',() => {
+        numObject.numOne = []
+        numObject.numTwo = []
+        numObject.numOperator = []
+    })
+ }
+ 
+function showNumOne(e){
+    let numOne = "";
+    for(i = 0; i < numObject.numOne.length; i++){
+        numOne += numObject.numOne[i]
+    }
+    finalNumObject.numOne = parseInt(numOne);
+    console.log(finalNumObject.numOne)
+    document.getElementById("numNew").innerText = finalNumObject.numOne;
+}
